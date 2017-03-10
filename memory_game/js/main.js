bcc68592798cbedea780e0d8b4a9f4bfc430ef8d
Sara Kevorkian <sdkevorkian@gmail.com>
2017-03-09 16:38:15 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,20 @@
+// when 1st card is clicked on...
+	//display card 
+	//store card value
+	//start timer for first card of game
+//when second card clicked on
+	//display card
+	//store card value
+	//if same as 1st card, keep open, score +1
+	//if not same, flip both over and start again
+//when all cards flipped
+	//end game, stop timer
+
+console.log("up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("Picked: " + cardOne + ", "+ cardThree);
