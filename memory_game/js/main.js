9d7fa3b440d287548f9cf3e51b264b8a557ecaa5
Sara Kevorkian <sdkevorkian@gmail.com>
2017-03-10 17:37:36 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,9 +12,23 @@
 
 console.log("up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("Picked: " + cardOne + ", "+ cardThree);
+//first card flipped
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+//second card flipped
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+// test if 2 cards in play, and if match
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
