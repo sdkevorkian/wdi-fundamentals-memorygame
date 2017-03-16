158c12b635ff2bcc989d3cd14349a3c90147fd4c
Sara Kevorkian <sdkevorkian@gmail.com>
2017-03-15 19:53:14 -0700

    added score, flipbacks, result disply, reset button. I think I'm finished?? :) SK

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,36 +23,51 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var score = 0;
+var result = document.getElementById("match-or-not");	// for changing the message after you match or not
+var clickedCards = document.getElementsByClassName("clicked"); // for keeping track of which cards have been clicked, so we can turn them over
+
+//flips back cards after theyve been flipped, could be fixed - see comment in checkForMatch
+var flipBack = function (){
+	for (var i = 0; i < clickedCards.length; i+=1){
+		clickedCards[i].setAttribute("src","images/back.png");
+	}
+};
+
+//slightly slower so players can see where the cards that didn't match were
+var delayedFlipBack = function(){
+	var timeoutID = window.setTimeout(flipBack, 800);
+}
 
 //function checks if a match is present, if not resets cardsInPlay
-
 var checkForMatch = function(){
-	var result = document.getElementById("match-or-not");
-	
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		result.textContent = "You found a match!";
+		score += 1;
+		document.getElementById("score").textContent = score;
 	} else {
 		result.textContent = "Sorry, try again.";
-
+		delayedFlipBack(); // this can be fixed- if you click on a non-match after having matched cards already on the board they all flip back
 	}
 	cardsInPlay = [];
 };
 
-//function whenever we click on a card to flip it over
-
+//whenever we click on a card to flip it over, displays card and holds result
 var flipCard = function() {
 	var cardId = this.getAttribute("data-id");
 	var card = cards[cardId];
 	this.setAttribute("src", card.cardImage);
+	this.setAttribute("class", "clicked");
+	console.log(this);
 	cardsInPlay.push(card.rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-		}
+	}
 };
 
 //initial function to create game board
 var createBoard = function(){
-	for (i = 0; i < cards.length; i++) {
+	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement("img");
 		cardElement.setAttribute("src", "images/back.png");
 		cardElement.setAttribute("data-id", i)
@@ -62,5 +77,14 @@ var createBoard = function(){
 	}
 };
 
-createBoard();
+//starts game over!
+var resetGame = function(){
+	score = 0;
+	document.getElementById("score").textContent = score;
+	result.textContent = "Ready for a new game??";
+	flipBack();
+};
 
+//begins program by creating board and waiting for reset 
+createBoard();
+document.querySelector("button").addEventListener("click", resetGame);
